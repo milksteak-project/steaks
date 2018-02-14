6a4a73e91cc94ce1df56ba9e709f3b87efb7f3ed
Vidir Alexander Jonsson <valex@valex.is>
2018-02-14 15:58:49 +0000

    testing new format

--- packages/dsniff/dsniff.sh
+++ packages/dsniff/dsniff.sh
@@ -1,67 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
+TMPDIR="$HOME/usr/tmp"
 
-DOWNLOAD_FOLDER=$HOME/usr/tmp
+PACKAGE="dsniff"
+VERSION="2.4b1-darwin_x86_64"
+
+LINK="https://github.com/sigma-1/$PACKAGE/archive/$VERSION.tar.gz"
+SOURCE="$PACKAGE-$VERSION"
+TARBALL="$SOURCE.tar.gz"
+
+DEPS="libnet libnids libpcap openssl"
+
+FLAGS="--with-libpcap=/usr/local/opt/libpcap --with-openssl=/usr/local/opt/openssl --with-libnet=/usr/local/opt/libnet --with-libnids=/usr/local/opt/libnids --without-x"
+PREFIX="--prefix=$HOME/usr --datarootdir=$HOME/usr/share --mandir=$HOME/usr/share/man"
 
 # -- Install dependencies
-DEPS="libnet libnids libpcap openssl"
-brew install $DEPS
-
-# -- Check and download the release
-echo -e "${RED}>>>>>>>> ${NC}Downloading dsniff-2.4b1-darwin_x86_64.tar.gz${NC}"
-test -e dsniff-2.4b1-darwin_x86_64.tar.gz || wget -O $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64.tar.gz https://github.com/sigma-1/dsniff/archive/2.4b1-darwin_x86_64.tar.gz -q --show-progress
-echo -e "${NC}dsniff-2.4b1-darwin_x86_64.tar.gz has been downloaded to:${NC}"
-sleep 1
-echo -e "${GREEN}> $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64.tar.gz${NC}"
-sleep 1
-
-# -- Unpack the release
-echo -e "${RED}>>>>>>>> ${NC}Unpacking dsniff-2.4b1-darwin_x86_64.tar.gz${NC}"
-cd $DOWNLOAD_FOLDER
-pv dsniff-2.4b1-darwin_x86_64.tar.gz | tar xz
-echo -e "${NC}dsniff-2.4b1-darwin_x86_64.tar.gz has been unpacked to:${NC}"
-echo -e "${GREEN}> $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64${NC}"
-
-# -- Build dsniff
-echo -e "${RED}>>>>>>>> ${NC}Building dsniff${NC}"
-cd $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64
-
-function build_dsniff() {
-	cd $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64
-	./configure --with-libpcap=/usr/local/opt/libpcap --with-openssl=/usr/local/opt/openssl --with-libnet=/usr/local/opt/libnet --with-libnids=/usr/local/opt/libnids --without-x --prefix=$HOME/usr --datarootdir=$HOME/usr/share --mandir=$HOME/usr/share/man
-	make
+function install_dependencies() {
+	brew install $DEPS
 }
+echo -e ">>>>> Installing dependencies..."
+install_dependencies &> /dev/null
 
-build_dsniff &> /dev/null
-
-sleep 1
-echo -e "${GREEN}Done!${NC}"
-
-# -- Install
-echo -e "${RED}>>>>>>>> ${NC}Installing dsniff${NC}"
-
-function install_dsniff() {
-	cd $DOWNLOAD_FOLDER/dsniff-2.4b1-darwin_x86_64
-	make install
+function fetch_package() {
+	# -- Fetch source tarball
+	test -e $TARBALL || wget -O $TMPDIR/$TARBALL $LINK -q --show-progress
+	# -- Unpack tarball
+	cd $TMPDIR ; pv $TARBALL | tar xz
 }
+echo -e ">>>>> Fetching sources..."
+fetch_package &> /dev/null
 
-install_dsniff &> /dev/null
+function install_package() {
+	cd $TMPDIR/$SOURCE
+	./configure $FLAGS $PREFIX
+	make ; make install
+}
+echo -e ">>>>> Installing package..."
+install_package &> /dev/null
 
-sleep 1
-echo -e "${GREEN}Done!${NC}"
-sleep 1
-echo -e "${NC}dsniff has been installed to:${NC}"
-sleep 1
-echo -e "${GREEN}> $HOME/usr${NC}"
-sleep 1
+function cleanup() {
+	cd $TMPDIR
+	rm $TARBALL
+	rm -rf $SOURCE
+}
+echo -e ">>>>> Cleaning up..."
+cleanup &> /dev/null
 
-# -- Cleanup
-cd $DOWNLOAD_FOLDER
-rm dsniff-2.4b1-darwin_x86_64.tar.gz
-rm -rf dsniff-2.4b1-darwin_x86_64
-
-printf '\e[1;31m%*s\e[m' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
-echo -e "${NC}"
+echo -e "$PACKAGE has been successfully installed!"
--- packages/dsniff/info.sh
+++ packages/dsniff/info.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m' # No Color
-
-if [ ! -f $HOME/usr/sbin/dsniff ]; then
-	echo -e "${RED}Package:${NC} dsniff (not installed)${NC}"
+if [ ! -f $HOME/usr/sbin/dsniff ] ; then
+	echo -e "Package: dsniff (not installed)"
 else
-	echo -e "${RED}Package:${GREEN} dsniff (installed)${NC}"
+	echo -e "Package: dsniff (installed)"
 fi
-echo -e "${RED}Version:${NC} 2.4b1${NC}"
-echo -e "${RED}Homepage:${NC} https://www.monkey.org/~dugsong/dsniff${NC}"
-
-echo -e "${RED}Description:${NC}"
-echo -e "${NC}dsniff is a collection of tools for network auditing and penetration testing. dsniff, filesnarf, mailsnarf, msgsnarf, urlsnarf, and webspy passively monitor a network for interesting data (passwords, e-mail, files, etc.). arpspoof, dnsspoof, and macof facilitate the interception of network traffic normally unavailable to an attacker (e.g, due to layer-2 switching). sshmitm and webmitm implement active monkey-in-the-middle attacks against redirected SSH and HTTPS sessions by exploiting weak bindings in ad-hoc PKI.${NC}"
-
-echo -e "${RED}Dependencies:"
-echo -e "${NC}libnet libnids libpcap openssl${NC}"
+echo
+echo -e "Version: 2.4b1"
+echo -e "Homepage: https://www.monkey.org/~dugsong/dsniff"
+echo
+echo -e "Description:"
+echo -e "dsniff is a collection of tools for network auditing and penetration testing."
+echo
+echo -e "Dependencies:"
+echo -e "libnet libnids libpcap openssl${NC}"
+echo
