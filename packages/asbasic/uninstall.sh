aa622dcb64acbeb3ebba169ecf98ad679bdd8c83
Vidir Alexander Jonsson <valex@valex.is>
2018-02-17 02:45:28 +0000

    asbasic: update uninstall script

--- packages/asbasic/uninstall.sh
+++ packages/asbasic/uninstall.sh
@@ -3,5 +3,7 @@
 # -- Uninstall
 echo -e ">>>>> Uninstalling asbasic..."
 rm $HOME/usr/bin/asbasic
+rm $HOME/usr/lib/libasbasic.a
+rm $HOME/usr/share/man/man1/asbasic.1
 rm -rf $HOME/usr/etc/packages/asbasic
 echo -e "asbasic has been successfully uninstalled!"
